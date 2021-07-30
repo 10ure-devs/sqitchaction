815482c1a5db8eb0cae90ce5ffaf38efb8dc53c5
Malcolm Monroe <malcolmian.monroe@gmail.com>
2021-07-30 13:43:33 -0700

    debugging

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,12 +2,13 @@
 set -e 
 
 echo "Navigating to $7"
-
 cd $7
 
 ls
 
+set -x
 PGUSER=$1 PGPASSWORD=$2 PGDATABASE=$3 PGHOST=$4 PGPORT=$5 SSLMODE=$6 sqitch deploy -v || exit 1
+set +x
 
 time=$(date)
 echo "::set-output name=time::$time"
