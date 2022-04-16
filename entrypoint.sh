1df06f32939aac34cc72f3409cb089e4a909ec5f
Malcolm Monroe <malcolm@10ure.com>
2022-04-16 11:32:51 +0200

    Remove verbose, can be added optionally as additional sqitch arg

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,7 +7,7 @@ cd $7
 ls
 
 set -x
-PGUSER=$1 PGPASSWORD=$2 PGDATABASE=$3 PGHOST=$4 PGPORT=$5 SSLMODE=$6 sqitch deploy -v $8 || exit 1
+PGUSER=$1 PGPASSWORD=$2 PGDATABASE=$3 PGHOST=$4 PGPORT=$5 SSLMODE=$6 sqitch deploy $8 || exit 1
 set +x
 
 time=$(date)
